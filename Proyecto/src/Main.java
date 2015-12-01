48ea9d77666dea6b782e8509a112ecd577319299
jonalonsov <jonalonso@opendeusto.es>
2015-12-01 14:10:33 +0100

    create table

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -11,6 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		BasesDeDatos.initBD("nuestrosDatos");
+		BasesDeDatos.crearTablaBDUsuario();
 		
 		principio frame = new principio();
 		frame.setVisible(true);
