52bd56ed06b187f184ccb4ea0764d2391228c3a1
jonalonsov <jonalonso@opendeusto.es>
2016-01-14 18:13:14 +0100

    metodo casillaTest

--- Proyecto/test/TestProyecto/GestorCasillasTest.java
+++ Proyecto/test/TestProyecto/GestorCasillasTest.java
@@ -18,6 +18,8 @@ public class GestorCasillasTest extends TestCase{
 	
 	Casilla casilla1;
 	Casilla casilla2;
+	GestorCasillas gc;
+	ArrayList<Casilla> arrayCasilla;
 			
 		
 		@Before
@@ -25,6 +27,8 @@ public class GestorCasillasTest extends TestCase{
 			
 			casilla1 = new Casilla(210, 295, "verde", 3);
 			casilla2 = new Casilla(1055, 130, "rojo", 1);
+			gc = new GestorCasillas();
+			arrayCasilla = new ArrayList<Casilla>();
 			
 		}
 		
@@ -73,19 +77,24 @@ public class GestorCasillasTest extends TestCase{
 		@Test
 		public void testcasillasList2() {
 			
-			GestorCasillas gc = new GestorCasillas();
-			ArrayList<Casilla> arrayCasilla = new ArrayList<Casilla>();
 			
-			gc.comenzarCasillas1();
 			
-			arrayCasilla = gc.casillasList1();
+			gc.comenzarCasillas2();
 			
-			int k1 = gc.casillasList1().size()+1;
+			arrayCasilla = gc.casillasList2();
 			
-			arrayCasilla.add(casilla1);
-			int k2 = arrayCasilla.size();
+			//int k1 = gc.casillasList1().size()+1;
 			
-			assertEquals(k1,k2);
+			//arrayCasilla.add(casilla1);
+			//int k2 = arrayCasilla.size();
+			
+			//assertEquals(k1,k2);
+			
+			int pos_x = casilla2.getPos_x();
+			int pos_x2 = arrayCasilla.get(1).getPos_x();
+			
+			
+			assertEquals(pos_x , pos_x2);
 			
 		}
 		
